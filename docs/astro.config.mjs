c4b4d5ba21becd84e9311a11a556d93b316a6a59
AlexK <kondaurov.dev@gmail.com>
2026-02-19 12:35:42 +0400

    chore: gtm added

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -37,6 +37,17 @@ export default defineConfig({
       },
       head: [
         {
+          tag: "script",
+          attrs: {
+            async: true,
+            src: "https://www.googletagmanager.com/gtag/js?id=G-1Y8J143871",
+          },
+        },
+        {
+          tag: "script",
+          content: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments)}gtag('js',new Date());gtag('config','G-1Y8J143871');`,
+        },
+        {
           tag: "meta",
           attrs: {
             property: "og:image",
