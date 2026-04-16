162c8d66c250d3b08235186fc6fc5806b4747299
AlexK <kondaurov.dev@gmail.com>
2026-04-16 12:56:16 +0400

    bump effortless [skip-ci]

--- docs/aws-resources.ts
+++ docs/aws-resources.ts
@@ -1,12 +1,11 @@
 import { defineStaticSite } from "effortless-aws";
 
 export const docsCDN = defineStaticSite({
-  dir: "dist",
+  dir: "docs/dist",
   build: "pnpm run build",
   domain: "tg-bot-sdk.website",
-  spa: false,
   seo: {
     sitemap: 'sitemap.xml',
     googleIndexing: '~/google-index-seo.json'
   }
-});
+}).build();
--- effortless.config.ts
+++ effortless.config.ts
@@ -3,5 +3,5 @@ import { defineConfig } from "effortless-aws";
 export default defineConfig({
   name: "tg-bot-docs",
   region: "eu-central-1",
-  handlers: ["aws-resources.ts"],
+  handlers: ["docs/aws-resources.ts"],
 });
